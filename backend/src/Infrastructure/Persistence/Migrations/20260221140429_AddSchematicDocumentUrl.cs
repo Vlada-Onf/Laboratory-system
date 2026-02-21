b90d9af930b86f9169624ff7c4c1391802a53fc7
Vlada-onf <vladyslava.onufriienko@oa.edu.ua>
2026-02-21 16:05:12 +0200

    Migration schematic

---
+++ backend/src/Infrastructure/Persistence/Migrations/20260221140429_AddSchematicDocumentUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSchematicDocumentUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "document_url",
+                table: "schematics",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "document_url",
+                table: "schematics");
+        }
+    }
+}
